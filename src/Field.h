745e6618fe10596478706b1d3e228ab2c2d6dbb5
WenderOk <wondergeorgy@yandex.ru>
2026-06-04 19:04:59 +0300

    ref: create class Field

---
+++ src/Field.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Block.h"
+#include <array>
+
+class Field
+{
+    static constexpr int WIDTH{10};
+    static constexpr int HEIGHT{20};
+    std::array<std::array<int, WIDTH>, HEIGHT> grid;
+public:
+    int clearLines();
+    void lockBlock(Block block);
+    bool hasCollision(Block block, int offsetX, int offsetY);
+};
+
