ff601b7b990ec8424a56cdab8252cfbdcf8ecc2a
WenderOk <wondergeorgy@yandex.ru>
2026-06-04 19:06:05 +0300

    ref: create InputHandler class

---
+++ src/InputHandler.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class InputHandler
+{
+public:
+    void initConsole();
+    void enableVirtualTerminal();
+    void restoreConsole();
+    void clearScreen();
+    #ifndef _WIN32
+        bool kbhit();
+        char getch();
+    #endif
+    void sleepMillis(int ms);
+};
