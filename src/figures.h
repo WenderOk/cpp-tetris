2391629e6843f6f14f608541f407cb3072f7e13f
WenderOk <wondergeorgy@yandex.ru>
2026-06-04 19:06:48 +0300

    ref: tranform old functions from headers into methods

--- src/figures.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int** rotateShape(int** shape, int height, int width, int& newHeight, int& newWidth);
-bool checkCollision(int x, int y, int** shape, int height, int width);
-void freeBlock(Block& block);
-void newBlock();
-void addBlockToField();
