4b780829cc7f820ce6ace7077802591d2ffe5e2e
WenderOk <wondergeorgy@yandex.ru>
2026-06-04 22:51:21 +0300

    feat: add getters in Field class

--- src/Field.h
+++ src/Field.h
@@ -10,7 +10,14 @@ class Field
     std::array<std::array<int, WIDTH>, HEIGHT> grid;
 public:
     int clearLines();
-    void lockBlock(Block block);
-    bool hasCollision(Block block, int offsetX, int offsetY);
+    void lockBlock(Block& block);
+    bool hasCollision(Block& block, int offsetX, int offsetY) const;
+
+    int getWidth() const
+    { return WIDTH; }
+    int getHeight()
+    { return HEIGHT; }
+    const std::array<std::array<int, WIDTH>, HEIGHT>& getGrid() const
+    { return grid; }
 };
 
