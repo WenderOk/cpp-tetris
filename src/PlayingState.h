fc5d7f5eb09b454fae15d8b5184693e884c93a34
WenderOk <wondergeorgy@yandex.ru>
2026-06-08 18:22:09 +0300

    feat: add fields for score, level and cleared lines and add getter for score and level

--- src/PlayingState.h
+++ src/PlayingState.h
@@ -8,16 +8,26 @@ class PlayingState: public GameState
 {
     Block currentBlock;
     std::chrono::steady_clock::time_point lastFallTime;
+
     int fallInterval;
+    int score;
+    int linesCleared;
+    int level;
     
     void spawnNewBlock(Game& game);
 public:
     PlayingState() 
-        : currentBlock(Block::createRandom(10)), 
-          lastFallTime(std::chrono::steady_clock::now()), 
-          fallInterval(500) 
+        : currentBlock{Block::createRandom(10)}, 
+          lastFallTime{std::chrono::steady_clock::now()}, 
+          fallInterval{500},
+          score{},
+          linesCleared{0},
+          level{1}
     {}
     void handleInput(Game& game) override;
     void update(Game& game, double deltaTime) override;
     void render(const Game& game) const override;
+
+    int getScore() const { return score; }
+    int getLevel() const { return level; }
 };
