3cfd6b59f7572500065ebd4270e0b392c2cbd775
WenderOk <wondergeorgy@yandex.ru>
2026-06-08 20:20:43 +0300

    feat: fill GameOverState class

--- src/GameOverState.h
+++ src/GameOverState.h
@@ -3,4 +3,13 @@
 #include "GameState.h"
 
 class GameOverState: public GameState
-{};
+{
+    int finalScore;
+public:
+    GameOverState(int score): finalScore(score)
+    {}
+
+    void handleInput(Game& game) override;
+    void update(Game& game, double deltaTime) override;
+    void render(const Game& game) const override;
+};
