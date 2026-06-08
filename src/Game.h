8ea745ad9f1b3e555d3de035c51676c7a1720b15
WenderOk <wondergeorgy@yandex.ru>
2026-06-08 17:43:00 +0300

    ref: add default constructor for Game class, add constant getters, change currentState field type to unique_ptr

--- src/Game.h
+++ src/Game.h
@@ -4,17 +4,31 @@
 #include "InputHandler.h"
 #include "Renderer.h"
 #include "GameState.h"
+#include <memory>
 
 class Game
 {
     Field field;
     Renderer renderer;
     InputHandler inputHandler;
-    GameState* currentState;
+    std::unique_ptr<GameState> currentState;
 public:
+    Game();
     void run();
-    void setState(GameState newState);
-    Field& getField();
-    Renderer& getRenderer();
-    InputHandler& getInputHandler();
+    void setState(std::unique_ptr<GameState>  newState);
+    
+    Field& getField()
+    { return field; }
+    const Field& getField() const
+    { return field; }
+
+    Renderer& getRenderer()
+    { return renderer; }
+    const Renderer& getRenderer() const
+    { return renderer; }
+
+    InputHandler& getInput()
+    { return inputHandler; }
+    const InputHandler& getInput() const
+    { return inputHandler; }
 };
