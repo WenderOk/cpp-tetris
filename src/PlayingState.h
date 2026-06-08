b42a352766c785a13bb125fe24641703b3a01352
WenderOk <wondergeorgy@yandex.ru>
2026-06-08 17:49:41 +0300

    feat: fill PlayingState class

--- src/PlayingState.h
+++ src/PlayingState.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include "Block.h"
 #include "GameState.h"
+#include <chrono>
 
 class PlayingState: public GameState
-{};
+{
+    Block currentBlock;
+    std::chrono::steady_clock::time_point lastFallTime;
+    int fallInterval;
+    
+    void spawnNewBlock(Game& game);
+public:
+    PlayingState() 
+        : currentBlock(Block::createRandom(10)), 
+          lastFallTime(std::chrono::steady_clock::now()), 
+          fallInterval(500) 
+    {}
+    void handleInput(Game& game) override;
+    void update(Game& game, double deltaTime) override;
+    void render(const Game& game) const override;
+};
