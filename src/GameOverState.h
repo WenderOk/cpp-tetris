ffe6a107e274666a68bad2dfb909b1b9e6f7fea3
WenderOk <wondergeorgy@yandex.ru>
2026-06-09 21:03:48 +0300

    feat: add highscore save to constructor

--- src/GameOverState.h
+++ src/GameOverState.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include "GameState.h"
+#include "Game.h"
+#include "HighScoreManager.h"
+#include "HighScoreManager.h"
 
 class GameOverState: public GameState
 {
     int finalScore;
 public:
     GameOverState(int score): finalScore(score)
-    {}
+    {
+        HighScoreManager::saveIfHigher(finalScore);
+    }
 
     void handleInput(Game& game) override;
     void update(Game& game, double deltaTime) override;
