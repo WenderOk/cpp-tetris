69376fde92a29dfff0c310ed86a541ebc7baa58c
WenderOk <wondergeorgy@yandex.ru>
2026-06-09 21:02:56 +0300

    feat: add best score logic, rename kbhit and getch

--- src/GameOverState.cpp
+++ src/GameOverState.cpp
@@ -5,9 +5,9 @@
 
 void GameOverState::handleInput(Game& game)
 {
-    if(!game.getInput().kbhit()) return;
+    if(!game.getInput().hasInput()) return;
 
-    char key{ game.getInput().getch() };
+    char key{ game.getInput().readChar() };
 
     if(key == 'r' || key == 'R')
         game.setState(std::make_unique<PlayingState>(game));
@@ -20,13 +20,16 @@ void GameOverState::update(Game& game, double deltaTime)
 
 void GameOverState::render(const Game& game) const {
     game.getRenderer().clearScreen();
+   
+     int highScore = HighScoreManager::load();
     
     std::cout << "\n\n";
-    std::cout << "   \e[0;101m==================================\e[0m\n";
-    std::cout << "   \e[0;101m=           GAME OVER            =\e[0m\n";
-    std::cout << "   \e[0;101m==================================\e[0m\n\n";
-    std::cout << "      YOUR FINAL SCORE: " << finalScore << "\n\n";
-    std::cout << "   \e[41mPress [R] to Restart              \e[0m\n";
-    std::cout << "   \e[41mPress [Q] to Quit                 \e[0m\n";
-    std::cout << "   \e[0;101m==================================\e[0m\n";
+    std::cout << "   \033[0;101m==================================\033[0m\n";
+    std::cout << "   \033[0;101m=           GAME OVER            =\033[0m\n";
+    std::cout << "   \033[0;101m==================================\033[0m\n\n";
+    std::cout << "      YOUR FINAL SCORE: " << finalScore << "\n";
+    std::cout << "      YOUR BEST SCORE: " << highScore << "\n\n";
+    std::cout << "   \033[41mPress [R] to Restart              \033[0m\n";
+    std::cout << "   \033[41mPress [Q] to Quit                 \033[0m\n";
+    std::cout << "   \033[0;101m==================================\033[0m\n";
 }
