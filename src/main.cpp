9adbbcf860e826e3fee64ef21fd2449f1f520451
WenderOk <wondergeorgy@yandex.ru>
2026-06-09 21:06:36 +0300

    ref: remove unused imports

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
-#ifdef _WIN32
-    #include <windows.h>
-    #include <conio.h>
-#endif
-
 #include "Game.h"
 
 int main() {
     Game game;
     game.run();
-    return 0; // Деструктор InputHandler автоматически восстановит консоль!
+    return 0;
 }
